rdepena/node-dualshock-controller

commit d53e877e3803618c2b689d8ad1134b15a6521259

Allow for passing of custom config object

lib/dualshock.js
@@ -5,7 +5,7 @@ var controller = require('./controller.js');
 // This is the app entry point.
 //  options you can pass:
 //  {
-//   config : "File from controllerConfigurations",
+//   config : "File from controllerConfigurations" or a JS object containing configuration,
 //   accelerometerSmoothing : true/false, this will activate motion/acelerometer output smoothing. true by default.
 //   analogStickSmoothing : true/false, this will activate analog thumb stick smoothing
 //  }
@@ -24,8 +24,12 @@ var dualShock = function (options) {
     //defaults analogStickSmoothing is turned off
     options.analogStickSmoothing = options.analogStickSmoothing === undefined ? false : options.analogStickSmoothing;
 
-    //need to load the config file with all the device specifics.
-    var controllerConfiguration = require('./../controllerConfigurations/' + options.config);
+    //use passed config or load from built-in configs
+    if (typeof options.config === "object") {
+      var controllerConfiguration = options.config;
+    } else {
+      var controllerConfiguration = require('./../controllerConfigurations/' + options.config);
+    }
 
     //loads the configuration;
     var ds = new controller(controllerConfiguration, options);

commit 2f93e78000a6c66aad499cd10fbc76e0e7868d4c

Fixes broken indentation introduced in d53e877e3803618c2b689d8ad1134b15a6521259
Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

lib/dualshock.js
@@ -28,9 +28,9 @@ var dualShock = function (options) {
 
     //use passed config or load from built-in configs
     if (typeof options.config === "object") {
-      controllerConfiguration = options.config;
+        controllerConfiguration = options.config;
     } else {
-      controllerConfiguration = require('./../controllerConfigurations/' + options.config);
+        controllerConfiguration = require('./../controllerConfigurations/' + options.config);
     }
 
     //loads the configuration;