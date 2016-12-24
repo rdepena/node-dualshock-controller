2315f0a3b1d66bab90b5e976c06be2946f50b96b
Ricardo De Pena <ricardo@isnull.co>
2016-12-24 11:17:15 -0500

    DS3/DS4 rumble and led

--- examples/consolePrintDualShock4.js
+++ examples/consolePrintDualShock4.js
@@ -2,11 +2,58 @@
 var dualShock = require('./../src/dualshock.js');
 
 var dualShock4 = dualShock({
-    config: "dualshock4-generic-driver"
+    config: "dualShock4",
+    forceNodeHid: true
 });
 
 //for a client implementation we do not need this, this is only to test the inputs.
-var controllerConfiguration = require('./../controllerConfigurations/dualshock4-generic-driver');
+var controllerConfiguration = require('./../controllerConfigurations/dualShock4');
 
 //init the print events
 var consolePrintEvents = require('./consolePrintControllerEvents')(dualShock4, controllerConfiguration);
+
+dualShock4.on("dpadup:press", () => {
+    dualShock4.setExtras({
+        red: 255
+    });
+});
+
+dualShock4.on("dpadright:press", () => {
+    dualShock4.setExtras({
+        green: 255
+    });
+});
+
+dualShock4.on("dpaddown:press", () => {
+    dualShock4.setExtras({
+        blue: 255
+    });
+});
+
+dualShock4.on("dpadleft:press", () => {
+    dualShock4.setExtras({
+        red: 255,
+        green: 255,
+        blue: 255
+    });
+});
+
+dualShock4.on("x:press", (d) => {
+    dualShock4.setExtras({
+        red: 255,
+        flashOn: 50,
+        flashOff: 10
+    });
+});
+
+dualShock4.on("r2:analog", (d) => {
+    dualShock4.setExtras({
+        rumbleRight: d
+    });
+});
+
+dualShock4.on("l2:analog", (d) => {
+    dualShock4.setExtras({
+        rumbleLeft: d
+    });
+});
